c3bfa02725944d8c3969385b7ec60f81befefaaf
pragyan69 <prajyanqw@gmail.com>
2024-04-24 22:59:35 +0530

    initial commit

---
+++ src/constants/index.jsx
@@ -0,0 +1,161 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+import { GlobeLock } from "lucide-react";
+
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+
+export const navItems = [
+  { label: "Features", href: "#" },
+  { label: "Workflow", href: "#" },
+  { label: "Pricing", href: "#" },
+  { label: "Testimonials", href: "#" },
+];
+
+export const testimonials = [
+  {
+    user: "John Doe",
+    company: "Stellar Solutions",
+    image: user1,
+    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+  },
+  {
+    user: "Jane Smith",
+    company: "Blue Horizon Technologies",
+    image: user2,
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  },
+  {
+    user: "David Johnson",
+    company: "Quantum Innovations",
+    image: user3,
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  },
+  {
+    user: "Ronee Brown",
+    company: "Fusion Dynamics",
+    image: user4,
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  },
+  {
+    user: "Michael Wilson",
+    company: "Visionary Creations",
+    image: user5,
+    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  },
+  {
+    user: "Emily Davis",
+    company: "Synergy Systems",
+    image: user6,
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  },
+];
+
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    text: "Real-Time Moderation",
+    description:
+      "Toxic Boss utilizes cutting-edge AI to scan and flag toxic comments in real-time, ensuring that online platforms remain safe and inclusive. By immediately identifying harmful language, it enables swift moderation actions, maintaining a positive community environment.",
+  },
+  {
+    icon: <Fingerprint />,
+    text: "Adaptive Learning",
+    description:
+      "Equipped with self-learning algorithms, Toxic Boss continuously improves its detection capabilities based on new data and user feedback. This adaptive approach enhances its accuracy over time, effectively keeping pace with the evolving nature of online communication and slang.",
+  },
+  {
+    icon: <ShieldHalf />,
+    text: "User-Centric Reporting",
+    description:
+      "Toxic Boss offers detailed analytics and reporting tools that allow moderators and platform administrators to understand the frequency and types of toxicity occurring on their platforms. This feature empowers users with actionable insights, aiding in strategic decisions to foster healthier interactions.",
+  }
+ 
+];
+
+export const checklistItems = [
+  {
+    title: "Automate Moderation Workflows",
+    description:
+      "Seamlessly integrate Toxic Boss' AI moderation into your platform, automating the detection and management of toxic comments to maintain a clean and safe online environment.",
+  },
+  {
+    title: "Accurate Detection with Confidence",
+    description:
+      "Trust in Toxic Boss' precision-engineered models to accurately identify toxic content, allowing you to focus on enhancing user interactions without the concern of manual content review.",
+  },
+  {
+    title: "Real-Time Reports on Community Health",
+    description:
+      "Toxic Boss' AI-driven tools significantly reduce the time required for content moderation, enabling moderators to efficiently handle higher volumes of user interactions with reduced effort.",
+  },
+  {
+    title: "Streamline Moderation Processes",
+    description:
+      "Toxic Boss' AI-driven tools significantly reduce the time required for content moderation, enabling moderators to efficiently handle higher volumes of user interactions with reduced effort.",
+  },
+];
+
+export const pricingOptions = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Pro",
+    price: "$10",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "$200",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
